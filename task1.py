af44f9acd31decf35b91e854f1a65d1997df9a33
Rogerlv51 <38689909+Rogerlv51@users.noreply.github.com>
2023-04-18 19:59:13 +0800

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cv2 as cv
+import pandas as pd
+import glob
+import os, sys
+from sklearn.preprocessing import normalize
+
+def get_cos_similar_matrix(v1, v2):
+    num = np.dot(v1, np.array(v2).T)  # 向量点乘
+    denom = np.linalg.norm(v1, axis=1).reshape(-1, 1) * np.linalg.norm(v2, axis=1)  # 求模长的乘积
+    res = num / denom
+    res[np.isneginf(res)] = 0
+    return 0.5 + 0.5 * res
+
+def main():
+    # 计算dataset文件夹中所有图的直方图
+    dataset_feat = []
+    # dataset_list = glob.glob('dataset/*.jpg')
+    dataset = os.listdir('dataset')
+    print(len(dataset))
+    for path in dataset:
+        img = cv.imread(os.path.join('dataset', path), 0)  # 读入灰度图模式
+        feat = cv.calcHist(np.array([img]), [0], None, [256], [0, 256]).flatten()
+        dataset_feat.append(feat)   # 记录所有dataset中的直方图信息
+
+    # 进行归一化
+    dataset_feat = np.array(dataset_feat)
+    dataset_feat = normalize(dataset_feat)   # 我感觉归一化是为了方便计算相似度
+
+    # 计算query文件夹中所有图像的直方图
+    query_feat = []
+    # query_list = glob.glob('query/*.jpg')
+    query = os.listdir('query')
+    print(len(query))
+    for path in query:
+        img = cv.imread(os.path.join('query', path), 0)
+        feat = cv.calcHist(np.array([img]), [0], None, [256], [0, 256]).flatten()
+        query_feat.append(feat)
+
+    # 进行归一化
+    query_feat = np.array(query_feat)
+    query_feat = normalize(query_feat)
+
+    # 计算每张query图片与dataset图片的颜色直方图相似度，这里采用最简单的办法就是直接计算两个向量的点积
+    
+    # dis = np.dot(query_feat, dataset_feat.T)   # 维度为2500,11141
+    # print(dis.shape)
+    cof = get_cos_similar_matrix(query_feat, dataset_feat)
+    print(cof.shape)
+    dataset = np.array(dataset)
+
+    # 生成提交结果
+    # 我们现在的目标是要在dataset中找到与query中每张图片最相似的图片，所以下面要按行取最大值找到在dataset中的索引
+    pd.DataFrame({
+        'source': [x for x in dataset[cof.argmax(1)]],
+        'query': [x for x in query]
+    }).to_csv('submit2.csv', index=None)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ task3_VIT.py
@@ -0,0 +1,78 @@
+from timm import create_model
+import torch
+import numpy as np
+import pandas as pd
+import os, sys
+import time
+from sklearn.preprocessing import normalize
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+vit_model = create_model('vit_base_patch32_224', pretrained=True).to(device)
+# VIT这边官方也是取的倒数第二层的特征向量
+
+from PIL import Image
+import torchvision.transforms as transforms
+
+def get_cos_similar_matrix(v1, v2):
+    num = np.dot(v1, v2.T)  # 向量点乘
+    denom = np.linalg.norm(v1, axis=1).reshape(-1, 1) * np.linalg.norm(v2, axis=1)  # 求模长的乘积
+    res = num / denom
+    res[np.isneginf(res)] = 0
+    return 0.5 + 0.5 * res
+
+# 定义预处理步骤
+preprocess = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+dataset = os.listdir('dataset')
+query = os.listdir('query')
+dataset_feat = []
+query_feat = []
+
+for data in dataset:
+    img = Image.open(os.path.join('dataset', data)).convert('RGB')
+    img_tensor = preprocess(img)
+    
+    img_batch = img_tensor.unsqueeze(0).to(device)
+    with torch.no_grad():
+        vit_model.eval()
+        data_feature = vit_model(img_batch).flatten()
+    dataset_feat.append(data_feature.cpu().numpy())
+
+dataset_feat = np.array(dataset_feat)
+dataset_feat = normalize(dataset_feat)
+
+print("dataset特征提取完毕")
+for q in query:
+    start = time.time()
+    q_img = Image.open(os.path.join('query', q)).convert('RGB')
+    # 预处理图像
+    q_tensor = preprocess(q_img)
+    # 将输入张量添加一个维度以表示批次大小
+    q_batch = q_tensor.unsqueeze(0).to(device)
+    with torch.no_grad():
+        vit_model.eval()
+        q_feature = vit_model(q_batch).flatten()
+    query_feat.append(q_feature.cpu().numpy())
+
+query_feat = np.array(query_feat)
+query_feat = normalize(query_feat)
+print(dataset_feat.shape)
+print(query_feat.shape)
+print("query特征提取完毕")
+
+# 计算余弦相似度
+cof = get_cos_similar_matrix(query_feat, dataset_feat)
+print(cof.shape)
+dataset = np.array(dataset)
+# 将结果写入csv文件
+pd.DataFrame({
+    'source': [x for x in dataset[cof.argmax(1)]],
+    'query': [x for x in query]
+}).to_csv('task3_VIT.csv', index=None)
